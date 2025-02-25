47a7cd7c2cdad22ddb4759049ad9b3fbc84ea406
Ashish Raj <rajjashish1500@gmail.com>
2025-02-25 23:17:31 +0530

    Initial commit

--- src/dashboard.js
+++ src/dashboard.js
@@ -3,7 +3,7 @@ import React from "react";
 function Welcome() {
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
-      <h1>Welcome to Green Tiger</h1>
+      <h1>Welcome to Green Tiger Application</h1>
       <p>You have successfully logged in!</p>
     </div>
   );
